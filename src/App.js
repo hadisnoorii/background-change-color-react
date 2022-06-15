f907b353c3800f68245fa6fcbb161a9922069f29
Hadis <hadisnoorim@gmail.com>
2022-06-15 12:46:33 +0430

    background changer

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const colors = [
+    "#D3E4CD",
+    "#99A799",
+    "#F2DDC1",
+    "#E2C2B9",
+    "#C37B89",
+    "#D1E8E4",
+    "#BCCC9A",
+    "#BFA2DB",
+    "#AEE1E1",
+    "#E8E46E",
+  ];
+  const [colorName, setColorName] = useState("");
+
+  const handleClick = (event, id) => {
+    const mainBg = colors[id];
+    setColorName(mainBg);
+  };
+
+  return (
+    <div className="App">
+      <main className="card" style={{ backgroundColor: colorName }}>
+        <header className="App-header" style={{ fontWeight: "bold" }}>
+          {colorName !== "" ? `Color Code: ${colorName}` : "Choose Color"}
+        </header>
+        <ul>
+          <li>
+            {colors.map((color, id) => (
+              <span
+                key={id}
+                style={{
+                  backgroundColor: color,
+                  borderColor: color === colorName ? "#16C79A" : "white",
+                }}
+                className="circle"
+                onClick={(event) => handleClick(event, id)}
+              ></span>
+            ))}
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
